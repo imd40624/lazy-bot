69cd4b0c4c06ba7126153dadebacd39c08ceb732
md imran <46713870+imd40624@users.noreply.github.com>
2019-04-21 16:13:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,167 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import youtube_dl
+import asyncio
+import queue
+import time
+import json
+import random
+import inspect
+import datetime
+import os
+from discord import Game
+from discord.utils import get
+
+Client = discord.client
+client = commands.Bot(command_prefix = 'bq.')
+Clientdiscord = discord.Client()
+evn=client.event
+cms=client.command(pass_context=True)
+
+async def picker():
+    mem_watching=['{} Users']
+    mem_watching=['Biquest']
+    mem_listening=['{} Users']
+    mem_playing=['Need upvotes to grow!']
+    ser_watch=['{} servers']
+    ser_listen=['Steveo']
+    ser_play=['bq.help | v1.0']
+    helps=['!help | for help','!help for help commands']
+
+    while True:
+        kind=random.randint(1,2)
+        if kind == 1:
+            members=0
+            for i in client.servers:
+                members+=len(i.members)
+            num = random.choice([1, 2, 3])
+            if num == 1:
+                await client.change_presence(game=discord.Game(name=random.choice(mem_playing).format(members), type=1))
+            if num == 2:
+                await client.change_presence(game=discord.Game(name=random.choice(mem_listening).format(members), type=2))
+            if num == 3:
+                await client.change_presence(game=discord.Game(name=random.choice(mem_watching).format(members), type=3))
+            await asyncio.sleep(random.choice([10, 10, 10, 10, 10, 10]))
+
+        if kind == 2:
+            num = random.choice([1, 2, 3])
+            if num == 1:
+                await client.change_presence(game=discord.Game(name=random.choice(ser_play).format(len(client.servers)), type=1))
+            if num == 2:
+                await client.change_presence(game=discord.Game(name=random.choice(ser_listen).format(len(client.servers)), type=2))
+            if num == 3:
+                await client.change_presence(game=discord.Game(name=random.choice(ser_watch).format(len(client.servers)), type=3))
+            await asyncio.sleep(random.choice([10, 10, 10, 10, 10, 10]))
+
+        if kind == 3:
+            await client.change_presence(game=discord.Game(name=random.choice(ser_watch).format(len(client.servers)), type=3))
+            await asyncio.sleep(random.choice([10, 10, 10, 10, 10, 10]))
+            
+            
+@client.event
+async def on_ready():
+    client.loop.create_task(picker())
+    print("{} has successfully booted and running!".format(client.user.name)) 
+    
+    
+    
+@client.command(pass_context=True)
+async def ping(ctx):
+    await client.say(":ping_pong: ping!! xSSS")
+    print ("user has pinged")   
+    
+    
+    
+@client.command(pass_context=True)
+async def userinfo(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=0xe67e22)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="ID", value=user.id, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.add_field(name="Created at", value=user.created_at)
+    embed.add_field(name="Nickname", value=user.nick)
+    embed.add_field(name="Bot", value=user.bot)
+    embed.set_thumbnail(url=user.avatar_url)
+    await client.say(embed=embed)  
+    
+    
+@client.command(Pass_Context=True) 
+async def invite():
+    embed=discord.Embed(title="Invite Me", description="Invite LaZy today!", color=0xff0000)
+    embed.set_author(name="Invite LaZy")
+    embed.set_footer(text="Made By DAKSH#0053") 
+    embed.add_field(name="Invite", value=f"[Click here for Link](https://discordapp.com/api/oauth2/authorize?client_id=569463328942850078&permissions=8&scope=bot)")
+    await client.say(embed=embed)
+    
+  
+@client.command(pass_context=True)
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await client.say('{0.name} joined in {0.joined_at}'.format(member)) 
+   
+   
+   
+@client.event
+async def on_member_remove(member):
+    server = member.server
+    channel = get(member.server.channels, name="join-leave")
+    s=discord.Embed(description="**{}** just left the server".format(member.name), colour=0xf84b50, timestamp=datetime.utcnow())
+    s.set_author(name=member, icon_url=member.avatar_url)
+    s.set_footer(text="User ID: {}".format(member.id))
+    await client.send_message(channel, embed=s)   
+   
+   
+   
+@client.command(pass_context=True, aliases=['server'])
+@commands.has_permissions(kick_members=True)
+async def membercount(ctx, *args):
+    if ctx.message.channel.is_private:
+        await bot.delete_message(ctx.message)
+        return
+
+    g = ctx.message.server
+
+    gid = g.id
+    membs = str(len(g.members))
+    membs_on = str(len([m for m in g.members if not m.status == Status.offline]))
+    users = str(len([m for m in g.members if not m.bot]))
+    users_on = str(len([m for m in g.members if not m.bot and not m.status == Status.offline]))
+    bots = str(len([m for m in g.members if m.bot]))
+    bots_on = str(len([m for m in g.members if m.bot and not m.status == Status.offline]))
+    created = str(g.created_at)
+    
+    em = Embed(title="Membercount")
+    em.description =    "```\n" \
+                        "Members:   %s (%s)\n" \
+                        "  Users:   %s (%s)\n" \
+                        "  Bots:    %s (%s)\n" \
+                        "Created:   %s\n" \
+                        "```" % (membs, membs_on, users, users_on, bots, bots_on, created)
+
+    await client.send_message(ctx.message.channel, embed=em)
+    await client.delete_message(ctx.message)
+   
+   
+   
+@client.command(pass_context=True)
+@commands.has_permissions(administrator=True)
+async def embed(ctx, *args):
+    if ctx.message.author.bot:
+      return
+    else:
+      argstr = " ".join(args)
+      r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+      text = argstr
+      color = discord.Color((r << 16) + (g << 8) + b)
+      await client.send_message(ctx.message.channel, embed=Embed(color = color, description=text))
+      await client.delete_message(ctx.message)   
+      
+      
+      
+      
+      
+      
+client.run(os.environ['BOT_TOKEN'])      
